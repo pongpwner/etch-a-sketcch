2831df4a8ddbe1006c407c23487b6511ea0769ea
EricShyu <ericsshyus@gmail.com>
2022-08-01 05:08:05 -0400

    add grid into the dom

---
+++ script.js
@@ -0,0 +1,24 @@
+const body= document.querySelector('body')
+const GRID_WIDTH=16;
+const GRID_HEIGHT=16;
+let gridArray=[];
+//create grid container
+let gridContainer= document.createElement('div');
+gridContainer.classList.add('grid-container')
+
+
+//create tile
+function createTile(){
+
+    let tile=document.createElement('div')
+    tile.classList.add('tile')
+    
+    return tile;
+}
+
+//fill grid-container
+for(i=0;i<(GRID_WIDTH*GRID_HEIGHT); i++){
+    gridArray.push(createTile())
+    gridContainer.appendChild(gridArray[i])
+}
+body.appendChild(gridContainer)
