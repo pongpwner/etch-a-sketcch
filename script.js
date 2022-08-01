6c1b75b2ec69b15311bdfaead270ebb1a6a51dd0
EricShyu <ericsshyus@gmail.com>
2022-08-01 07:06:52 -0400

    change modal to prompt and remove border from tiles

--- script.js
+++ script.js
@@ -2,15 +2,48 @@ const body= document.querySelector('body')
 let GRID_WIDTH=16;
 let GRID_HEIGHT=16;
 let gridArray=[];
-//button to open modal to control grid size
-let button= document.createElement('button');
 
-button.innerText='choose grid size'
-button.classList.add('modal-button')
-button.addEventListener('click',openModal)
-body.appendChild(button)
-//create grid container
+//prompt button
+let promptButton= document.createElement('button');
+promptButton.innerText='choose grid size'
+promptButton.classList.add('prompt-button')
+promptButton.addEventListener('click',gridPrompt)
+body.appendChild(promptButton)
+function gridPrompt(){
+    let gridSize=prompt("choose a grid size between 1 and 100")
+    console.log(gridSize)
+    if(gridSize>100){
+        GRID_WIDTH=100;
+        GRID_HEIGHT=100;
+        removeGrid()
+createGrid()
+        return
+    }
+    if(gridSize<1){
+        GRID_WIDTH=1;
+        GRID_HEIGHT=1;
+        removeGrid()
+createGrid()
+        return
+    }
+    GRID_WIDTH=gridSize;
+ GRID_HEIGHT=gridSize;
+
+removeGrid()
+createGrid()
+    
+}
+
+//button to open modal to control grid size
+// let button= document.createElement('button');
+
+// button.innerText='choose grid size'
+// button.classList.add('modal-button')
+// button.addEventListener('click',openModal)
+// body.appendChild(button)
+// //create grid container
 function createGrid(){
+    console.log(GRID_HEIGHT +'x'+GRID_WIDTH)
 let gridContainer= document.createElement('div');
 gridContainer.classList.add('grid-container')
 gridContainer.style.setProperty('grid-template-columns', 'repeat(' + GRID_HEIGHT+ ', 1fr)')
@@ -37,77 +70,78 @@ createGrid();
 
 
 //modal
-function openModal(){
+// function openModal(){
  
-    //...
-    let modalContainer=document.createElement('div');
-    //modalContainer.classList.add('modal-container');
+//     //...
+//     let modalContainer=document.createElement('div');
+//     //modalContainer.classList.add('modal-container');
 
-    let closeButton=document.createElement('button')
-    closeButton.classList.add('close-button')
-    closeButton.innerText="X"
-    closeButton.addEventListener('click',()=> modalContainer.remove());
+//     let closeButton=document.createElement('button')
+//     closeButton.classList.add('close-button')
+//     closeButton.innerText="X"
+//     closeButton.addEventListener('click',()=> modalContainer.remove());
 
-    let widthLabel=document.createElement('label');
-    widthLabel.innerText="Width";
+//     let widthLabel=document.createElement('label');
+//     widthLabel.innerText="Width";
 
-    let heightLabel=document.createElement('label');
-    heightLabel.innerHTML='Height'
-    let widthInput=document.createElement('input')
-    widthInput.type='number';
-    widthInput.addEventListener('change', (e)=>setGridWidth(e))
-    let heightInput=document.createElement('input')
-    heightInput.type='number';
-    heightInput.addEventListener('change', (e)=>setGridHeight(e))
-    let createGridButton=document.createElement('button')
-    createGridButton.classList.add('create-grid-button')
-    createGridButton.innerText='Create Grid.'
-    createGridButton.addEventListener('click',()=>{
-        removeGrid();
-        createGrid()});
+//     let heightLabel=document.createElement('label');
+//     heightLabel.innerHTML='Height'
+//     let widthInput=document.createElement('input')
+//     widthInput.type='number';
+//     widthInput.addEventListener('change', (e)=>setGridWidth(e))
+//     let heightInput=document.createElement('input')
+//     heightInput.type='number';
+//     heightInput.addEventListener('change', (e)=>setGridHeight(e))
+//     let createGridButton=document.createElement('button')
+//     createGridButton.classList.add('create-grid-button')
+//     createGridButton.innerText='Create Grid.'
+//     createGridButton.addEventListener('click',()=>{
+//         removeGrid();
+//         createGrid()});
 
-    modalContainer.appendChild(closeButton)
-    modalContainer.appendChild(widthLabel)
-    modalContainer.appendChild(widthInput)
-    modalContainer.appendChild(heightLabel)
-    modalContainer.appendChild(heightInput)
-    modalContainer.appendChild(createGridButton)
-    body.appendChild(modalContainer)
-}
-function setGridWidth(e){
-    if(e.target.value>100){
-        alert("number must be between 1 and 100")
-        GRID_WIDTH=100;
-        e.target.value=100;
-        return
-    }
+//     modalContainer.appendChild(closeButton)
+//     modalContainer.appendChild(widthLabel)
+//     modalContainer.appendChild(widthInput)
+//     modalContainer.appendChild(heightLabel)
+//     modalContainer.appendChild(heightInput)
+//     modalContainer.appendChild(createGridButton)
+//     body.appendChild(modalContainer)
+// }
+// function setGridWidth(e){
+//     if(e.target.value>100){
+//         alert("number must be between 1 and 100")
+//         GRID_WIDTH=100;
+//         e.target.value=100;
+//         return
+//     }
     
-    if(e.target.value<1){
-        GRID_WIDTH=1;
-        e.target.value=1;
-        alert("number must be between 1 and 100")
-        return
-    }
-GRID_WIDTH=e.target.value;
-}
-function setGridHeight(e){
-    if(e.target.value>100){
-        alert("number must be between 1 and 100")
-        GRID_HEIGHT=100;
-        e.target.value=100;
-        return
-    }
+//     if(e.target.value<1){
+//         GRID_WIDTH=1;
+//         e.target.value=1;
+//         alert("number must be between 1 and 100")
+//         return
+//     }
+// GRID_WIDTH=e.target.value;
+// }
+// function setGridHeight(e){
+//     if(e.target.value>100){
+//         alert("number must be between 1 and 100")
+//         GRID_HEIGHT=100;
+//         e.target.value=100;
+//         return
+//     }
     
-    if(e.target.value<1){
-        GRID_HEIGHT=1;
-        e.target.value=1;
-        alert("number must be between 1 and 100")
-        return
-    }
-    GRID_HEIGHT=e.target.value;
+//     if(e.target.value<1){
+//         GRID_HEIGHT=1;
+//         e.target.value=1;
+//         alert("number must be between 1 and 100")
+//         return
+//     }
+//     GRID_HEIGHT=e.target.value;
 
-}
+// }
 function removeGrid(){
+    console.log('rem')
     let grid=document.querySelector('.grid-container')
     grid.remove();
 }
